658a2acfbeb762f1c907bbf265c40fba6e483779
fangye66 <fangye.yf@gmail.com>
2021-06-15 01:25:01 -0400

    Create itvshow.ts

---
+++ src/app/itvshow.ts
@@ -0,0 +1,15 @@
+
+export interface Itvshow {
+  name: string,
+  description: string,
+  language: String,
+  network: string,
+  rating: string,
+  schedule: Date,
+  runtime: number, 
+  country: string,
+  image: string
+
+
+
+}
