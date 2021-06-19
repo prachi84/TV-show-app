dc5b89b854afd340ef0bc12b5ef372b9e7bf0a1f
prachi84 <prachi8384@gmail.com>
2021-06-19 14:49:42 -0700

    created 2 interface

---
+++ src/app/itvshowdata.ts
@@ -0,0 +1,29 @@
+export interface Itvshowdata {
+  show:[{
+    name:string,
+    language:string
+  }],
+  genres:{
+    0:string,
+    1:string,
+    2:string,
+    runtime:Number,
+    premiered:Date
+},
+schedule:{
+  time:number,
+}
+rating:{
+  average:number
+},
+network:{
+  name:string
+},
+country:{
+  name:string
+},
+image	:string,
+summary:string,
+
+
+}
