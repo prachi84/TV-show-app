e5490f2f54ec4e319e9ec2ef573606203e5c3aba
Quynh <71189807+quynth@users.noreply.github.com>
2021-06-19 19:46:09 -0700

    Added first part of 2nd Service

--- src/app/tvshow.service.ts
+++ src/app/tvshow.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TvshowService {
-
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+  get TVContainer(show_name: string) {
+    return this.httpClient
+      .get<Itvshowdata>(`http://api.tvmaze.com/search/shows?q=${show_name}`)
+      .pipe();
+  }
 }
