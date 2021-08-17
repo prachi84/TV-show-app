5485518b11bd8aba6d26a2545ab4d892de06de9e
i67ahmed <i67ahmed@gmail.com>
2021-08-17 14:27:13 -0400

    Dashboard component set up

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GetAllShows } from '../services/getallshows.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  
+
+  constructor(public AllShows: GetAllShows) { }
 
   ngOnInit(): void {
   }
